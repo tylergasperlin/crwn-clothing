76b211dd6bf9b31bf5d6a5e891811a7be825df60
Tyler <gasperlint@gmail.com>
2019-11-28 22:22:13 -0600

    refactor project file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './components/homepage/homepage-component'
+import HomePage from './pages/homepage/homepage-component'
 import {Route, Switch} from 'react-router-dom'
 
 const HatsPage = () =>(
