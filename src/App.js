1b493a0727f5afa664c86c75fbab3bc0aa01ef5c
Tyler <gasperlint@gmail.com>
2019-11-29 00:45:55 -0600

    add signin form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/homepage/homepage-component'
 import {Route, Switch} from 'react-router-dom'
 import ShopPage from './pages/shop/shop-component'
 import Header from './components/header/header-component'
+import SignInSignOutPqge from './pages/signin-signout/signin-singout-component'
 
 
 function App() {
@@ -13,6 +14,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={HomePage}></Route>
           <Route exact path="/shop" component={ShopPage}></Route>
+          <Route exact path="/signin" component={SignInSignOutPqge}></Route>
+
         </Switch>
     </div>
 
---
+++ src/pages/signin-signout/signin-singout-component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SignIn from '../../components/signin/signin-component'
+import './signin-signout-styles.scss'
+
+const SignInSignOutPqge = () => (
+    <div className='sign-in-sign-up'>
+        <SignIn></SignIn>
+    </div>
+
+
+)
+
+export default SignInSignOutPqge;
