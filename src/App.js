34706d5f638d04df1b5f38fe0e422ead59add79c
Tyler <gasperlint@gmail.com>
2019-11-29 00:11:25 -0600

    add custom header

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import './App.css';
 import HomePage from './pages/homepage/homepage-component'
 import {Route, Switch} from 'react-router-dom'
 import ShopPage from './pages/shop/shop-component'
+import Header from './components/header/header-component'
 
 
 function App() {
   return (
     <div>
-      <Switch>
-        <Route exact path="/" component={HomePage}></Route>
-        <Route exact path="/shop" component={ShopPage}></Route>
-      </Switch>
+      <Header></Header>
+        <Switch>
+          <Route exact path="/" component={HomePage}></Route>
+          <Route exact path="/shop" component={ShopPage}></Route>
+        </Switch>
     </div>
 
  
