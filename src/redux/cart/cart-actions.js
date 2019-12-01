cac414704f6b3298c3c2ad4f0e1c5bfef31e81c0
Tyler <gasperlint@gmail.com>
2019-12-01 12:30:24 -0800

    add ability to remove items with x on checkout

--- src/redux/cart/cart-actions.js
+++ src/redux/cart/cart-actions.js
@@ -10,3 +10,9 @@ export const addItem = item => ({
 
 
 })
+
+export const clearItemFromCart = item => ({
+    type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+    payload: item
+
+})
--- src/redux/cart/cart-reducer.js
+++ src/redux/cart/cart-reducer.js
@@ -19,6 +19,13 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 //action payload is item we want to add
                 cartItems: addItemToCart(state.cartItems, action.payload) 
             };
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return{
+                ...state,
+                //filter returns anyhting that is true
+                //this will give us a new array of everything but what we are trying to clear
+                cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
+            }
         default: 
             return state;
     }
