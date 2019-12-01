7b00d0b4850df015587ab7b7b1e6d168b001e1ca
Tyler <gasperlint@gmail.com>
2019-12-01 11:59:56 -0800

    when user clicks button to go to checkout hide the cart dropdown

--- src/components/cart-dropdown/cart-dropdown-component.jsx
+++ src/components/cart-dropdown/cart-dropdown-component.jsx
@@ -9,8 +9,10 @@ import CustomButtom from '../custom-button/custom-button-component'
 import CartItem from '../cart-item/cart-item-component'
 
 import { selectCartItems } from '../../redux/cart/cart-selectors'
+import {toggleCartHidden} from '../../redux/cart/cart-actions'
 
-const CartDropdown = ({cartItems, history}) =>(
+
+const CartDropdown = ({cartItems, history, dispatch}) =>(
     <div className='cart-dropdown'>
         <div className='cart-items'/>
         {
@@ -18,7 +20,11 @@ const CartDropdown = ({cartItems, history}) =>(
                 cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem}/>))
                 : (<span className='empty-message'> Your cart is empty</span>)
         }
-        <CustomButtom onClick={()=>history.push('/checkout')}>
+        <CustomButtom 
+            onClick={()=>
+                {history.push('/checkout');
+                dispatch(toggleCartHidden())}
+            }>
             GO TO CHECKOUT
         </CustomButtom>
     </div>
@@ -33,4 +39,7 @@ const mapStateToProps = createStructuredSelector({
 //since using functional component we use with router to get access to history
 //with history we can navigate user to the checkout page
 //this gives us access to history in state which we use in const Cartdropdown = cartitems,history
+
+//connect passes dispatch into this automatically so we do not need to create mapDispatchToProps
+//we will structure out dispatch in the functional component
 export default withRouter(connect(mapStateToProps)(CartDropdown));
