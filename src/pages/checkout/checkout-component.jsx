84abd6ed2c69a1a26b7228bbf1490e3aaf42f59e
Tyler <gasperlint@gmail.com>
2019-12-01 11:50:33 -0800

    add checkout page and total price selector

--- src/pages/checkout/checkout-component.jsx
+++ src/pages/checkout/checkout-component.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
-import './checkout-styles.scss'
+import React from "react";
+import {connect} from 'react-redux'
+import {createStructuredSelector} from 'reselect'
 
-const CheckoutPage = () => (
-    <div>
-        CHECKOUT PAGE
+import { selectCartItems, selectCartTotal} from '../../redux/cart/cart-selectors'
+import "./checkout-styles.scss";
+
+const CheckoutPage = ({cartItems, total}) => (
+  <div className="checkout-page">
+    <div className="checkout-header">
+      <div className="header-block">
+        <span>Product</span>
+      </div>
+      <div className="header-block">
+        <span>Description</span>
+      </div>
+      <div className="header-block">
+        <span>Quantity</span>
+      </div>
+      <div className="header-block">
+        <span>Price</span>
+      </div>
+      <div className="header-block">
+        <span>Remove</span>
+      </div>
     </div>
+    {
+        cartItems.map(cartItem => cartItems.name)
+    }
+    <div className='total'>
+        <span>TOTAL: ${total}</span>
+    </div>
+  </div>
+);
 
-)
+const mapStateToProps = createStructuredSelector({
+    cartItems: selectCartItems,
+    total: selectCartTotal
+})
 
-export default CheckoutPage;
+export default connect(mapStateToProps)(CheckoutPage);
--- src/redux/cart/cart-selectors.js
+++ src/redux/cart/cart-selectors.js
@@ -22,6 +22,15 @@ export const selectCartItemsCount = createSelector(
 
 )
 
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems => 
+        cartItems.reduce(
+            (accumulatedQuantity, cartItem) => 
+                accumulatedQuantity + (cartItem.quantity * cartItem.price), 0)
+
+)
+
 
 //notes//
     //reselect makes it so that the component does not rerender when there are state changes unrelated
