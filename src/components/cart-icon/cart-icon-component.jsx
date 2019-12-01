4d533ce22cc8b71c1a112fe689669f597a497548
Tyler <gasperlint@gmail.com>
2019-12-01 09:48:48 -0800

    add cart count to component

--- src/components/cart-icon/cart-icon-component.jsx
+++ src/components/cart-icon/cart-icon-component.jsx
@@ -4,11 +4,11 @@ import { toggleCartHidden} from '../../redux/cart/cart-actions'
 import './cart-icon-styles.scss'
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
 
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'> 0 </span>
+        <span className='item-count'> {itemCount} </span>
     </div>
 
 )
@@ -18,4 +18,14 @@ const mapDispatchToProps = dispatch => ({
 
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = ({cart:{cartItems}}) => ({
+    //reduce will execute a function  for each value in an array
+    //the function will add cartitem.quantity to the accumulated quantity for each c in cartitems
+    //this is a selector = we use a slice of state to calculate a value = calc quanitiy of 
+    ////items in cart based on state
+    itemCount: cartItems.reduce(
+        (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
