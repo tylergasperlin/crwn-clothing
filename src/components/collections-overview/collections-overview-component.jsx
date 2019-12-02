0f701c9bee1223c326556961c3f290c3947e2a0f
Tyler <gasperlint@gmail.com>
2019-12-01 19:53:33 -0800

    add ability to select collections for preview; fix css error on collection page

--- src/components/collections-overview/collections-overview-component.jsx
+++ src/components/collections-overview/collections-overview-component.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {createStructuredSelector} from 'reselect'
-import {selectCollections} from '../../redux/shop/shop-selector'
+import {selectCollectionsForPreview} from '../../redux/shop/shop-selector'
 
 import CollectionPreview from '../collection-preview/collection-preview-component'
 
@@ -18,7 +18,7 @@ const CollectionsOverview = ({collections}) =>(
 
 
 const mapStateToProps = state => createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsForPreview
 
 })
 
--- src/redux/shop/shop-selector.js
+++ src/redux/shop/shop-selector.js
@@ -7,6 +7,15 @@ export const selectCollections = createSelector(
     shop => shop.collections
 )
 
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    //get array of items that we are trying to get
+    //map the keys of collections ==mens/womens/hats/jackets
+    //get value of collection object at the key
+    collections => Object.keys(collections).map(key=>collections[key])
+
+)
+
 //returns a function that takes state and runs through the method
 //create selector is from reselect and allows us to selectively update state
 //whatever uses this selector will need to pass state to the returned function
