d43fe49909be8ad3a8db7e73dfa2fd42c6c4e7db
Tyler <gasperlint@gmail.com>
2019-12-01 19:00:21 -0800

    add ability to get a collection of clothes based on the url params

--- src/pages/collection/collection-component.jsx
+++ src/pages/collection/collection-component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import {connect} from 'react-redux'
 import CollectionItem from '../../components/collection-item/collection-item-component'
+import {selectCollection} from '../../redux/shop/shop-selector'
 import './collection-styles.scss'
 
 //we have access to match since the Collection page is within route in shop-component
-const CollectionPage = ({match}) => {
-    console.log(match.params.collectionId)
+const CollectionPage = ({collection}) => {
+    console.log(collection)
     return(
         <div className='collection-page'>
             <h2>COLLECTION PAGE</h2>
@@ -13,4 +15,13 @@ const CollectionPage = ({match}) => {
     )
 }
 
-export default CollectionPage;
+//ownprops is the props of the component we are wrapping in connect ==CollectionPage here
+const mapStateToProps = (state, ownProps)=>({
+    //this selector requires the a part of the state based on the url param 
+    //select collection returns a function => then state is passed into the function
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+
+
+})
+
+export default connect(mapStateToProps)(CollectionPage);
--- src/redux/shop/shop-selector.js
+++ src/redux/shop/shop-selector.js
@@ -1,8 +1,29 @@
 import {createSelector} from 'reselect'
 
+const COLLECTION_ID_MAP = {
+    //these correspond with tbe ids from the directory-reducer
+    hats: 1,
+    sneakers: 2,
+    jackets: 3,
+    women: 4, 
+    men: 5
+}
+
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
+)
+
+//returns a function that takes state and runs through the method
+//create selector is from reselect and allows us to selectively update state
+//whatever uses this selector will need to pass state to the returned function
+export const selectCollection = collectionUrlParam => createSelector(
+    [selectCollections],
+    //find collection id matching the url parameter of our collection id map
+    collections => collections.find(
+    collection => collection.id ===COLLECTION_ID_MAP[collectionUrlParam]
+    )
+
 )
