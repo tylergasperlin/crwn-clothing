961a4b2c9255c2581b206e94e0e7aac8e62567ec
Tyler <gasperlint@gmail.com>
2019-12-01 17:35:25 -0800

    create category component and add dynamic routing

--- src/pages/shop/shop-component.jsx
+++ src/pages/shop/shop-component.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
-
+import {Route} from 'react-router-dom'
 import CollectionsOverview from '../../components/collections-overview/collections-overview-component'
+import CategoryPage from '../../pages/category/category-component'
 
-const ShopPage = ({collections}) => (
+//match is a property of route (match location and history are available)
+const ShopPage = ({match}) => {
+    console.log(match)
+    return(
+        <div className='shop-page'>
+            <Route exact path={`${match.path}`} component={CollectionsOverview}/>
+            <Route path={`${match.path}/:categoryId`} component={CategoryPage}/>
+        </div>
 
-    <div className='shop-page'>
-        <CollectionsOverview/>
-    </div>
+    )
+
+}
+
+
     
-)
+
 
 
 export default ShopPage;
