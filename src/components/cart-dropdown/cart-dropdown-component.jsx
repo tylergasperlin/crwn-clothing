ada89ac37f48a974e926f3a2f34f163528f5ae06
Tyler <gasperlint@gmail.com>
2019-12-01 21:32:52 -0800

    fix cart dropdown css error

--- src/components/cart-dropdown/cart-dropdown-component.jsx
+++ src/components/cart-dropdown/cart-dropdown-component.jsx
@@ -14,12 +14,13 @@ import {toggleCartHidden} from '../../redux/cart/cart-actions'
 
 const CartDropdown = ({cartItems, history, dispatch}) =>(
     <div className='cart-dropdown'>
-        <div className='cart-items'/>
+        <div className='cart-items'>
         {
             cartItems.length ? (
                 cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem}/>))
                 : (<span className='empty-message'> Your cart is empty</span>)
         }
+        </div>
         <CustomButtom 
             onClick={()=>
                 {history.push('/checkout');
