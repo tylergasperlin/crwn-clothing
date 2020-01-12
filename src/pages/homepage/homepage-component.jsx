44a871c31df8f913694997a30316bd711acd2506
Tyler Gasperlin <gasperlint@gmail.com>
2020-01-12 09:33:44 -0800

    implement styled components

--- src/pages/homepage/homepage-component.jsx
+++ src/pages/homepage/homepage-component.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import './homepage-styles.scss'
 import Directory from '../../components/directory/directory-component'
+import {HomePageContainer} from './homepage-styles'
 
 const HomePage = (props) => {
     return(
-        <div className='homepage'>
-            <Directory></Directory>
-        </div>
+        <HomePageContainer>
+            <Directory/>
+        </HomePageContainer>
 
     )
 
